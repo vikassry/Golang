62a72f791539dbc7ae8ad228b8f95db8280078a3
vikassry <vikassry@gmail.com>
2016-07-22 15:28:46 +0530

    learning if_else control structure

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,20 @@
 package control_structures
 
+import (
+    "strconv"
+)
+
+
 func fizzBuzz(i int32) string {
-	return ""
+	if i % 3 == 0 &&  i % 5 == 0 {
+		return "FizzBuzz"
+	}
+	if i % 3 == 0 {
+		return "Fizz"
+	}
+	if i % 5 == 0 {
+		return "Buzz"
+	}
+	t := strconv.FormatInt(int32(i))
+	return t
 }
