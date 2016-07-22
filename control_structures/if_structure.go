28e5a32f8e09741ff9ae6af35c044848faf9043c
vikassry <vikassry@gmail.com>
2016-07-22 15:34:45 +0530

    Fixed converting int to string

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,20 +1,18 @@
 package control_structures
 
 import (
-    "strconv"
+	"fmt"
 )
 
-
 func fizzBuzz(i int32) string {
-	if i % 3 == 0 &&  i % 5 == 0 {
+	if i%3 == 0 && i%5 == 0 {
 		return "FizzBuzz"
 	}
-	if i % 3 == 0 {
+	if i%3 == 0 {
 		return "Fizz"
 	}
-	if i % 5 == 0 {
+	if i%5 == 0 {
 		return "Buzz"
 	}
-	t := strconv.FormatInt(int32(i))
-	return t
+	return fmt.Sprintf("%v", i)
 }
