0a64f5b8b6f4ac7b138fbe0860ded0a7e907a0e7
vikassry <vikassry@gmail.com>
2016-07-22 15:51:05 +0530

    Fixed the switch control structure

--- control_structures/switch_structure.go
+++ control_structures/switch_structure.go
@@ -6,5 +6,16 @@ const multiply string = "*"
 const divide = "/"
 
 func calculate(op string, arg1, arg2 float32) float32 {
-	return 0.0
+	switch op {
+
+	case add:
+		return arg1 + arg2
+	case subtract:
+		return arg1 - arg2
+	case multiply:
+		return arg1 * arg2
+	case divide:
+		return arg1 / arg2
+	}
+	return arg1
 }
