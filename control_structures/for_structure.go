98683004a28f477d0d54e475fd8071744f5f8dab
vikassry <vikassry@gmail.com>
2016-07-22 16:18:07 +0530

    Solve the FOR control structure collatzChain

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,15 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	i := 0
+	for n != 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = (n * 3) + 1
+		}
+		i++
+	}
+	return i
+
 }
