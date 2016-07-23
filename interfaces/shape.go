45ccccd8406ed652f2c7486f7c6325441298615c
vikassry <vikassry@gmail.com>
2016-07-23 10:30:41 +0530

    Playing with interfaces

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,29 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Square struct {
+	side int
+}
+
+func (s Square) Area() int {
+	return s.side * s.side
+}
+
+type Rectangle struct {
+	length  int
+	breadth int
+}
+
+func (r Rectangle) Area() int {
+	return r.length * r.breadth
+}
+
+type Hybrid struct {
+	s Shape
+	r Shape
+}
+
+func (h Hybrid) Area() int {
+	return h.s.Area() + h.r.Area()
+}
