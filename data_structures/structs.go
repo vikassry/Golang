8d76af595e6ce17484a38a05a4f78ffd1830e4b6
vikassry <vikassry@gmail.com>
2016-07-25 21:47:41 +0530

    LinkedList implemented using structs

--- data_structures/structs.go
+++ data_structures/structs.go
@@ -2,12 +2,33 @@ package data_structures
 
 // Linked List Implementation
 
-type Node struct{}
-
-func Add(elem int) {
-	return
+type Node struct {
+	value int
+	next  *Node
 }
 
-func Search(elem int) *Node {
+type LinkedList struct {
+	head   *Node
+	tail   *Node
+	length int
+}
+
+func (ll *LinkedList) Add(elem int) {
+	new_node := &Node{elem, nil}
+	if ll.head == nil && ll.head == nil {
+		ll.head = new_node
+	} else {
+		ll.tail.next = new_node
+	}
+	ll.tail = new_node
+	ll.length++
+}
+
+func (ll LinkedList) Search(elem int) *Node {
+	for walker := ll.head; walker != nil; walker = walker.next {
+		if walker.value == elem {
+			return walker
+		}
+	}
 	return nil
 }
--- data_structures/structs_test.go
+++ data_structures/structs_test.go
@@ -1,15 +1,18 @@
 package data_structures
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestContainsElementsAddedToLinkedList(t *testing.T) {
-	Add(1)
-	Add(2)
-	Add(3)
+	linkedList := new(LinkedList)
+	linkedList.Add(1)
+	linkedList.Add(2)
+	linkedList.Add(3)
 
 	for i := 1; i <= 3; i++ {
-		if Search(i) == nil {
-			t.Errorf("Should have contained a node with value %d", i)
+		if linkedList.Search(i) == nil {
+			t.Errorf("Should have contained a node with value %d but got %v", i, linkedList.Search(i))
 		}
 	}
 }
